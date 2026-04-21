732dae9abf10c3088c16a26a0be5faddac030afc
annieha <hajh5447@naver.com>
2026-04-21 21:16:48 +0900

    feat: 응답 구조 및 에러 처리

---
+++ src/main/java/com/goggles/common/exception/UnAuthorizedException.java
@@ -0,0 +1,12 @@
+package com.goggles.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UnAuthorizedException extends CustomException {
+    public UnAuthorizedException() {
+        this("로그인이 필요한 서비스입니다.");
+    }
+    public UnAuthorizedException(String message) {
+        super( message, HttpStatus.UNAUTHORIZED);
+    }
+}
