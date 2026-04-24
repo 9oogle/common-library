a29e8be693746c1f12783fd0cb5b76f0421fdb9f
hyew0nn <hyewon-0607@naver.com>
2026-04-24 12:49:32 +0900

    fix: AsyncConfigurer 중복 등록으로 컨텍스트 초기화 실패 위험 해결

--- src/main/java/com/goggles/config/event/EventConfig.java
+++ src/main/java/com/goggles/config/event/EventConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 
 @Configuration
-public class EventConfig implements AsyncConfigurer {
+public class EventConfig {
 
 	@Bean
 	public Events events(ApplicationEventPublisher eventPublisher) {
