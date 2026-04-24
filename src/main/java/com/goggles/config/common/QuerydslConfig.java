9063a079c1c05eb10b4726d34cce30d785104307
hyew0nn <hyewon-0607@naver.com>
2026-04-24 14:47:01 +0900

    refactor: QuerydslConfig 외부 서비스 설정 충돌 방지

--- src/main/java/com/goggles/config/common/QuerydslConfig.java
+++ src/main/java/com/goggles/config/common/QuerydslConfig.java
@@ -2,19 +2,24 @@ package com.goggles.config.common;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(name = "jakarta.persistence.EntityManagerFactory")
+@ConditionalOnBean(EntityManagerFactory.class)
 public class QuerydslConfig {
 
   @PersistenceContext
   private EntityManager entityManager;
-
+ 
   @Bean
+  @ConditionalOnMissingBean(JPAQueryFactory.class)
   public JPAQueryFactory jpaQueryFactory() {
     return new JPAQueryFactory(entityManager);
   }
