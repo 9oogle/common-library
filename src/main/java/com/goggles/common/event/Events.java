f1f050b145fbad2227d7e043e2a3308f4ec5f05f
annieha <hajh5447@naver.com>
2026-04-28 11:48:25 +0900

    fix: trigger static 함수로 변경

--- src/main/java/com/goggles/common/event/Events.java
+++ src/main/java/com/goggles/common/event/Events.java
@@ -1,13 +1,21 @@
 package com.goggles.common.event;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 
 @RequiredArgsConstructor
 public class Events {
 	private final ApplicationEventPublisher eventPublisher;
+	private static ApplicationEventPublisher publisher;
 
-	public void trigger(String correlationId, String domainType, String eventType, Object payload) {
-		eventPublisher.publishEvent(new OutboxEvent(correlationId, domainType, eventType, payload));
+	@PostConstruct
+	public void init() {
+		publisher = this.eventPublisher;
+	}
+
+	public static void trigger(String correlationId, String domainType, String eventType,
+			Object payload) {
+		publisher.publishEvent(new OutboxEvent(correlationId, domainType, eventType, payload));
 	}
 }
